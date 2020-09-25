2fe092ffee8a10c899f74a3e8a2aa44ed2c273a4
Arlecuno <palchinskij19@gmail.com>
2020-09-25 15:13:16 +0300

    modefi modal and add animations

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,6 +1,8 @@
 const modals = () => {
 
-   function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+   let btnPressed = false;
+
+   function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
       const trigger = document.querySelectorAll(triggerSelector),
          modal = document.querySelector(modalSelector),
          close = document.querySelector(closeSelector),
@@ -13,8 +15,15 @@ const modals = () => {
                e.preventDefault();
             }
 
+            btnPressed = true;
+
+            if (destroy) {
+               item.remove();
+            }
+
             windows.forEach(item => {
                item.style.display = 'none';
+               item.classList.add('animated', 'fadeIn');
             });
 
             modal.style.display = 'block';
@@ -26,6 +35,7 @@ const modals = () => {
       close.addEventListener('click', () => {
          windows.forEach(item => {
             item.style.display = 'none';
+
          });
          modal.style.display = 'none';
          document.body.classList.remove('modal-open');
@@ -33,7 +43,7 @@ const modals = () => {
       });
 
       modal.addEventListener('click', (e) => {
-         if (e.target === modal && closeClickOverlay) {
+         if (e.target === modal) {
             windows.forEach(item => {
                item.style.display = 'none';
             });
@@ -57,23 +67,8 @@ const modals = () => {
          if (!display) {
             document.querySelector(selector).style.display = 'block';
             document.body.classList.add('modal-open');
-         }
-      }, time);
-   }
-
-   function showModalByTime(selector, time) {
-      setTimeout(function () {
-         let display;
-
-         document.querySelectorAll('[data-modal]').forEach(item => {
-            if (getComputedStyle(item).display !== 'none') {
-               display = "block";
-            }
-         });
-
-         if (!display) {
-            document.querySelector(selector).style.display = 'block';
-            document.body.style.overflow = "hidden";
+            let scroll = calcScroll();
+            document.body.style.marginRight = `${scroll}px`;
          }
       }, time);
    }
@@ -93,9 +88,18 @@ const modals = () => {
       return scrollWidth;
    }
 
+   function openByScroll(selector) {
+      window.addEventListener('scroll', () => {
+         if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+            document.querySelector(selector).click();
+         }
+      });
+   }
+
    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-
+   bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+   openByScroll('.fixed-gift');
    showModalByTime('.popup-consultation', 5000);
 
 };
