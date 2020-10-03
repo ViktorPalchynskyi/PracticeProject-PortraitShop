4a5c34012631ab8256ec36118e3d4a2098b2fc3e
Arlecuno <palchinskij19@gmail.com>
2020-10-03 16:47:41 +0300

    added filter

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
+import filter from './modules/filter';
+
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -19,4 +21,5 @@ window.addEventListener('DOMContentLoaded', () => {
    checkTextInputs('[name="message"]');
    showMoreStyles('.button-styles', '.styles-2');
    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+   filter();
 });
